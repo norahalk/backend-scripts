4654df8f4eaed0be9c8df2da855c3249815f119a
Nourah Alkhaldi <norahmane@gmail.com>
2024-08-02 17:09:22 +0200

    Architecture folder parsing

--- app.py
+++ app.py
@@ -28,25 +28,40 @@ def extract_and_parse_folders(directory):
             parsed_result = parse_folder_name(item)
             if parsed_result:
                 version, flavor, date = parsed_result
+                architectures = []
+                for sub_item in os.listdir(item_path):
+                    sub_item_path = os.path.join(item_path, sub_item)
+                    if os.path.isdir(sub_item_path):
+                        architectures.append(sub_item)
                 if version not in parsed_folders:
                     parsed_folders[version] = []
-                parsed_folders[version].append((flavor, date))
+                parsed_folders[version].append((flavor, date, architectures))
     return parsed_folders
 
-@app.route('/api/folders', methods=['GET'])
+@app.route('/folders', methods=['GET'])
 def get_folders():
-    directory = '../Desktop/IBs'
+    directory = '../Desktop/package-info-viewer'
     parsed_folders = extract_and_parse_folders(directory)
     
     # Organize data for easier consumption by the frontend
     data = {}
-    for version, flavor_date_list in parsed_folders.items():
-        dates = defaultdict(list)
-        for flavor, date in flavor_date_list:
-            dates[date].append(flavor)
+    for version, flavor_date_arch_list in parsed_folders.items():
+        dates = defaultdict(lambda: defaultdict(list))
+        for flavor, date, architectures in flavor_date_arch_list:
+            dates[date][flavor] = architectures
         data[version] = dates
     
     return jsonify(data)
+
+@app.route('/packages/<ib>/<date>/<flavor>/<architecture>', methods=['GET'])
+def get_packages(ib, date, flavor, architecture):
+    directory = f'../Desktop/package-info-viewer/{ib}_{flavor}_{date}/{architecture}'
+    package_file = os.path.join(directory, 'cmssw-ib.json')
+    if os.path.exists(package_file):
+        with open(package_file) as f:
+            packages = f.read()
+        return packages
+    return jsonify([]), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
